77db37d752b18ed78da6291b7e4ec823c8cf9013
rajashekar <raja.shekar818181@gmail.com>
2022-12-14 16:53:44 +0530

    updating ./roboshop_shell

---
+++ 06.cart.sh
@@ -0,0 +1,18 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
+rm -rf cart
+unzip /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
+
+sed -i -e 's/REDIS_ENDPOINT/redis.rajashekar.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.rajashekar.online/' /home/roboshop/cart/systemd.service
+
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl restart cart
+systemctl enable cart
