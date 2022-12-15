71643bb8fb0ea7e0b9e0f5b187ad1a4fd13ef14f
rajashekar <raja.shekar818181@gmail.com>
2022-12-15 21:04:35 +0530

    updating ./roboshop_shell

--- 03.Catalogue.sh
+++ 03.Catalogue.sh
@@ -1,17 +1,4 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-cd /home/roboshop
-rm -rf catalogue
-unzip /tmp/catalogue.zip
-mv catalogue-main catalogue
-cd /home/roboshop/catalogue
-npm install
+COMPONENT=catalogue
+source common.sh
 
-sed -i -e 's/MONGO_DNSNAME/mongodb.devops69.online/' systemd.service
-
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl restart catalogue
-systemctl enable catalogue
+NODEJS
--- common.sh
+++ common.sh
@@ -15,3 +15,60 @@ PRINT(){
 
 LOG=/tmp/$COMPONENT.log
 rm -rf $LOG
+
+NODEJS(){
+  PRINT "Install NodeJS Repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG
+  STAT $?
+
+  PRINT "Install NodeJS"
+  yum install nodejs -y &>>$LOG
+  STAT $?
+
+  PRINT "Adding Application User"
+  id roboshop &>>$LOG
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>$LOG
+  fi
+  STAT $?
+
+  PRINT "Downloading App Content"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+  STAT $?
+
+  PRINT "Remove Previous Versions of App"
+  cd /home/roboshop &>>$LOG
+  rm -rf ${COMPONENT} &>>$LOG
+  STAT $?
+
+  PRINT "Extracting App Content"
+  unzip -o /tmp/${COMPONENT}.zip &>>$LOG
+  STAT $?
+
+  mv ${COMPONENT}-main ${COMPONENT} &>>$LOG
+  cd ${COMPONENT} &>>$LOG
+
+  PRINT "Install NodeJS Dependencies for App"
+  npm install &>>$LOG
+  STAT $?
+
+  PRINT "Configure Endpoints for systemD Configuration"
+  sed -i -e 's/REDIS_ENDPOINT/redis.rajashekar.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.rajashekar.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
+  STAT $?
+
+  PRINT "SetUp systemD service"
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG
+  STAT $?
+
+  PRINT "Reload systemd"
+  systemctl daemon-reload &>>$LOG
+  STAT $?
+
+  PRINT "Restart Cart"
+  systemctl restart ${COMPONENT} &>>$LOG
+  STAT $?
+
+  PRINT "Enable Cart Service"
+  systemctl enable ${COMPONENT} &>>$LOG
+  STAT $?
+}
