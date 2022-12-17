871b330692a2e6bed5892d48c0d7c28616077026
rajashekar <raja.shekar818181@gmail.com>
2022-12-17 08:51:54 +0530

    updating ./roboshop_shell

--- mysql.sh
+++ mysql.sh
@@ -1,31 +1,32 @@
-if [-z "$1"]; then
+if [ -z "$1" ]; then
   echo Input argument Password is needed
-  exit
+  exit 1
 fi
 
+COMPONENT=mysql
+source common.sh
 ROBOSHOP_MYSQL_PASSWORD=$1
 
-
-
-PRINT "Downloading MySql Repo File"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+PRINT "Downloading MySQL Repo File"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG
 STAT $?
 
-PRINT "Disable MySql version 8 repo"
-dnf module disable mysql
+PRINT "Disable MySQL 8 Version repo"
+dnf module disable mysql -y &>>$LOG
 STAT $?
 
-PRINT "Install MySql"
-yum install mysql-community-server -y
+PRINT "Install MySQL"
+yum install mysql-community-server -y &>>$LOG
 STAT $?
 
-PRINT "Enable MySql Service"
-systemctl enable mysqld
+PRINT "Enable MySQL Service"
+systemctl enable mysqld &>>$LOG
 STAT $?
 
-PRINT "Start MySql Service"
-systemctl restart mysqld
+PRINT "Start MySQL Service"
+systemctl restart mysqld &>>$LOG
 STAT $?
+
 
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG
 if [ $? -ne 0 ]
@@ -35,4 +36,21 @@ then
   cat /tmp/root-pass-sql  | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" &>>$LOG
 fi
 
-cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+PRINT "Uninstall Validate Plugin Password"
+echo "show plugins" |  mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} | grep validate_password &>>$LOG
+if [ $? -eq 0 ]; then
+  echo " uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}   &>>$LOG
+fi
+STAT $?
+
+APP_LOC=/tmp
+CONTENT=mysql-main
+DOWNLOAD_APP_CODE
+
+cd mysql-main &>>$LOG
+
+PRINT "Load Shipping Schema"
+mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql &>>$LOG
+STAT $?
+
+
