13b19e7a880d45a9929b07e15d99c3b6e67fd91b
rajashekar <raja.shekar818181@gmail.com>
2022-12-17 13:29:12 +0530

    updating ./roboshop_shell

--- create-roboshop-servers.sh
+++ create-roboshop-servers.sh
@@ -24,13 +24,13 @@ create_ec2() {
 
 
 ## Main Program
-AMI_ID=$(aws ec2 describe-images --filters= "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 if [ -z "${AMI_ID}" ]; then
   echo "AMI_ID not found"
   exit 1
 fi
 
-SGID=$(aws ec2 describe-security-groups --filters= Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 if [ -z "${SGID}" ]; then
   echo "Given Security Group does not exit"
   exit 1
