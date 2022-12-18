0b32c2bea661836e1eac9631f876b238d45d3cf7
rajashekar <raja.shekar818181@gmail.com>
2022-12-18 19:02:17 +0530

    updating ./roboshop_shell

--- frontend.sh
+++ frontend.sh
@@ -14,6 +14,12 @@ mv frontend-main/static/* .
 PRINT "Copy Roboshop Configuration File"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
+
+
+PRINT "Update RoboShop Configuration"
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.devopsb69.online/'  -e '/user/ s/localhost/dev-user.devopsb69.online/' -e '/cart/ s/localhost/dev-cart.devopsb69.online/' -e '/shipping/ s/localhost/dev-shipping.devopsb69.online/' -e '/payment/ s/localhost/dev-payment.devopsb69.online/' /etc/nginx/default.d/roboshop.conf
+STAT $?
+
 PRINT "Enable Nginx Service"
 systemctl enable nginx &>>$LOG
 STAT $?
