35ada8fcace22506af79b15e0f44f406aa75fe39
rajashekar <raja.shekar818181@gmail.com>
2022-12-18 11:48:05 +0530

    updating ./roboshop_shell

--- common.sh
+++ common.sh
@@ -1,58 +1,65 @@
-STAT(){
+STAT() {
   if [ $1 -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
-    echo -e "\e[32mFAILURE\e[0m"
+    echo -e "\e[31mFAILURE\e[0m"
     echo Check the error in $LOG file
     exit
   fi
 }
 
-PRINT(){
-  echo "--------$1--------">>${LOG}
+PRINT() {
+  echo " --------------------  $1 ------------------" >>${LOG}
   echo -e "\e[33m$1\e[0m"
 }
 
 LOG=/tmp/$COMPONENT.log
-rm -rf $LOG
+rm -f $LOG
 
-DOWNLOAD_APP_CODE(){
-  #this if condition: if app_user is not empty; then deal with that
+DOWNLOAD_APP_CODE() {
   if [ ! -z  "$APP_USER" ]; then
-      PRINT "Adding Application User"
-      id roboshop &>>$LOG
-      if [ $? -ne 0 ]; then
-        useradd roboshop &>>$LOG
-      fi
-      STAT $?
+    PRINT "Adding Application User"
+    id roboshop &>>$LOG
+    if [ $? -ne 0 ]; then
+      useradd roboshop &>>$LOG
+    fi
+    STAT $?
   fi
 
-  PRINT "Downloading App Content"
-  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+  PRINT "Download App Content"
+  curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/roboshop-devops-project/$COMPONENT/archive/main.zip" &>>$LOG
   STAT $?
 
-  PRINT "Remove Previous Versions of App"
+  PRINT "Remove Previous Version of App"
   cd $APP_LOC &>>$LOG
-  rm -rf ${CONTENT} &>>$LOG
+  rm -rf $CONTENT &>>$LOG
   STAT $?
 
   PRINT "Extracting App Content"
-  unzip -o /tmp/${COMPONENT}.zip &>>$LOG
+  unzip -o /tmp/$COMPONENT.zip &>>$LOG
   STAT $?
 }
 
 SYSTEMD_SETUP() {
   PRINT "Configure Endpoints for SystemD Configuration"
-  sed -i -e 's/MONGO_DNSNAME/dev-mongodb.devopsb69.online/' -e 's/REDIS_ENDPOINT/dev-redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/dev-catalogue.devopsb69.online/' -e 's/MONGO_ENDPOINT/dev-mongodb.devopsb69.online/' -e 's/CARTENDPOINT/dev-cart.devopsb69.online/' -e 's/DBHOST/dev-mysql.devopsb69.online/' -e 's/AMQPHOST/dev-rabbitmq.devopsb69.online/' -e 's/CARTHOST/dev-cart.devopsb69.online/' -e 's/USERHOST/dev-user.devopsb69.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
+  sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
   mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
   STAT $?
 
+  PRINT "Reload SystemD"
+  systemctl daemon-reload &>>$LOG
+  STAT $?
+
   PRINT "Restart ${COMPONENT}"
-  systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
+  systemctl restart ${COMPONENT} &>>$LOG
+  STAT $?
+
+  PRINT "Enable ${COMPONENT} Service"
+  systemctl enable ${COMPONENT} &>>$LOG
   STAT $?
 }
 
-NODEJS(){
+NODEJS() {
   APP_LOC=/home/roboshop
   CONTENT=$COMPONENT
   APP_USER=roboshop
@@ -65,17 +72,18 @@ NODEJS(){
   STAT $?
 
 
-
   DOWNLOAD_APP_CODE
 
-  mv ${COMPONENT}-main ${COMPONENT} &>>$LOG
-  cd ${COMPONENT} &>>$LOG
+  mv ${COMPONENT}-main ${COMPONENT}
+  cd ${COMPONENT}
 
-  PRINT "Install NodeJS Dependencies for App"
+  PRINT "Install NodeJS Dependencies for App "
   npm install &>>$LOG
   STAT $?
 
   SYSTEMD_SETUP
+}
+
 
 JAVA() {
   APP_LOC=/home/roboshop
